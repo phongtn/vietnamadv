b87b1b715167ad5f0e9d4586a475ca0c95b493f9
PhongTn <phongtn.group1@gmail.com>
2025-04-20 13:25:35 +0700

    remove unused import

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import { useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import './utils/i18n';
